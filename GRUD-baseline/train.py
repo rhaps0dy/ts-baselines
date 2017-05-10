39a21c3c23a4db70ac648aa5c4ca4330f88b146d
Adrià Garriga <adria.garriga@gmail.com>
2017-05-10 12:34:08 +0200

    Added smarter saving

--- GRUD-baseline/train.py
+++ GRUD-baseline/train.py
@@ -34,7 +34,7 @@ flags.DEFINE_string('model', 'GRUD', 'the model to use')
 flags.DEFINE_string('log_level', 'INFO', 'logging level')
 
 FILENAME_FLAGS = ['learning_rate', 'batch_size', 'hidden_units',
-                  'hidden_layers', 'dropout']
+                  'hidden_layers', 'dropout', 'layer_norm']
 
 FLAGS = tf.app.flags.FLAGS
 log = logging.getLogger(__name__)
@@ -115,7 +115,7 @@ def main(_):
     train_step = m.train_step()
 
     # Model checkpoints and graphs
-    saver = tf.train.Saver(max_to_keep=0)
+    saver = tf.train.Saver(max_to_keep=5)
     sess.run(tf.global_variables_initializer())
     if load_file:
         saver.restore(sess, load_file)
@@ -126,6 +126,7 @@ def main(_):
     validation_summary = tf.summary.scalar('validation/loss', loss_ph)
 
     if FLAGS.command == 'train':
+        min_loss_mean = 99999.
         learning_rate = FLAGS.learning_rate
         epoch_steps = (len(training_data)+FLAGS.batch_size-1) // FLAGS.batch_size
         log.info("Each epoch has {:d} steps.".format(epoch_steps))
@@ -149,9 +150,11 @@ def main(_):
             result = sess.run([validation_summary], {loss_ph: loss_mean})
             summary_writer.add_summary(result[0], summary_t)
 
-            save_path = saver.save(sess, save_model_file, global_step=summary_t)
-            log.info("Model saved in file: {:s}, validation loss {:.4f}"
-                        .format(save_path, loss_mean))
+            if loss_mean < min_loss_mean*1.1:
+                save_path = saver.save(sess, save_model_file, global_step=summary_t)
+                log.info("Model saved in file: {:s}, validation loss {:.4f}"
+                            .format(save_path, loss_mean))
+            min_loss_mean = min(min_loss_mean, loss_mean)
     elif FLAGS.command == 'test':
         if not os.path.exists(FLAGS.out_file):
             with open(FLAGS.out_file, 'w') as f:
