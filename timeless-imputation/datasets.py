bb3b78c5016d28fed761e20275b61ec843d50d9a
Adrià Garriga <adria.garriga@gmail.com>
2017-07-16 14:28:16 +0100

    Added more datasets and measuring metrics

--- timeless-imputation/datasets.py
+++ timeless-imputation/datasets.py
@@ -2,14 +2,17 @@ from rpy2.robjects.packages import importr
 import rpy2.robjects
 import rpy2.robjects.pandas2ri
 import rpy2.robjects.numpy2ri
-rpy2.robjects.pandas2ri.activate()
-rpy2.robjects.numpy2ri.activate()
-
 import numpy as np
 import pickle_utils as pu
 import pandas as pd
 import collections
+import utils
+import os
+import collections
+import unittest
 
+rpy2.robjects.pandas2ri.activate()
+rpy2.robjects.numpy2ri.activate()
 py2ri = rpy2.robjects.pandas2ri.py2ri
 ri2py = rpy2.robjects.pandas2ri.ri2py
 R = rpy2.robjects.r
@@ -18,33 +21,66 @@ base = importr("base")
 R_utils = importr("utils")
 mlbench = importr("mlbench")
 
-import utils
-import os
-
 __all__ = ['datasets', 'benchmark', 'memoize']
+
 
 def datasets():
     columns_excluded = {
-        "Ionosphere": ["V2", "Class"],
-        "BostonHousing": ["medv", "chas"],
-        #"BreastCancer": ["Class"],
-        #"DNA": ["Class"],
+        "Ionosphere": ["V2"],
+        "BostonHousing": [],
+        "BreastCancer": [],
+        "DNA": [],
+        "Soybean": [],
+        "Servo": [],
     }
     dfs = {}
     for name, excluded in columns_excluded.items():
-
         R_utils.data(name)
-        dfs[name] = (ri2py(base.as_data_frame(R[name]))
-                     .drop(excluded, axis=1))
-        if name == "Ionosphere":
-            dfs[name].V1 = dfs[name].V1.astype(np.float)
+        df = utils.df_ri2py(base.as_data_frame(R[name]))
+        assert all(df[k].dtype.name in ['category', 'float64', 'int32']
+                   for k in df.keys())
+        dfs[name] = df
     return dfs
+
 
 def memoize(f):
     @pu.memoize("{0:s}.pkl.gz")
     def memoized_f(_path, *args, **kwargs):
         return f(*args, **kwargs)
     return memoized_f
+
+def percentage_falsely_classified(amputed_data, full_data, imputed_data):
+    def key_filter(f):
+        df = full_data
+        return list(filter(lambda k: f(df[k].dtype), df.keys()))
+
+    def count_wrong_classifications(truth, attempts):
+        attempts = np.stack(attempts)
+        assert truth.shape[0] == attempts.shape[1]
+        assert len(truth.shape) == 1
+        wrong = 0
+        for t, a in zip(iter(truth), iter(attempts.T)):
+            wrong += (t != collections.Counter(a)
+                        .most_common(1)[0][0])
+        return wrong, len(truth)
+
+    cat_keys = key_filter(lambda dt: dt.name == 'category')
+    int_keys = key_filter(lambda dt: dt == np.int32)
+    cat_mask = amputed_data[cat_keys].isnull().values
+    wrong, total = count_wrong_classifications(
+        full_data[cat_keys].values[cat_mask],
+        list(i[cat_keys].values[cat_mask] for i in imputed_data))
+    int_mask = (amputed_data[int_keys].values == utils.NA_INT32)
+    wrong_, total_ = count_wrong_classifications(
+        full_data[int_keys].values[int_mask],
+        list(i[int_keys].values[int_mask] for i in imputed_data))
+
+    print("outs", wrong, total, wrong_, total_)
+
+    return {'PFC': 0 if total==0 or total_==0 else (wrong+wrong_)/(total+total_),
+            'PFC_cat': 0 if total==0 else wrong/total,
+            'PFC_int': 0 if total_==0 else wrong_/total_}
+
 
 def benchmark(impute_methods, datasets, path="impute_benchmark"):
     if not os.path.exists(path):
@@ -62,71 +98,133 @@ def benchmark(impute_methods, datasets, path="impute_benchmark"):
                     (memoize(utils.mcar_total), 'MCAR_total'),
                     (memoize(do_mcar_rows), 'MCAR_rows')]:
                 for proportion in [.1, .3, .5, .7, .9]:
-                    amputed_name = '{:s}_{:s}_{:.1f}'.format(
-                            data_name, ampute_fun_name, proportion)
-                    amputed_data = ampute_fun(
-                        os.path.join(path, 'amputed_'+amputed_name),
-                        full_data,
-                        proportion)
-                    imputed_data = impute_f(
-                        os.path.join(path, 'imputed_{:s}_{:s}'.format(
-                            algo_name, amputed_name)),
-                        amputed_data)
+                    for norm_type in ['mean_std', 'min_max']:
+                        amputed_name = '{:s}_{:s}_{:.1f}'.format(
+                                data_name, ampute_fun_name, proportion)
+                        amputed_data = ampute_fun(
+                            os.path.join(path, 'amputed_'+amputed_name),
+                            full_data,
+                            proportion)
+                        _data, moments = utils.normalise_dataframes(
+                            amputed_data, full_data, method=norm_type)
+                        _ad, _fd = _data
 
-                    table[algo_name]['RMSE_best'][data_name][ampute_fun_name]\
-                        [str(proportion)] = utils.mean_rmse(
-                            np.isnan(amputed_data.values),
-                            full_data.values,
-                            list(d.values for d in imputed_data))
+                        _id = impute_f(
+                            os.path.join(path, 'imputed_{:s}_{:s}'.format(
+                                algo_name, amputed_name)), _ad, full_data=_fd)
+                        imputed_data = utils.unnormalise_dataframes(moments, _id)
+                        # Normalised RMSE:
+                        # For normalised RMSE, we take mean_std normalisation
+                        # over the missing values only
+                        numerical_keys = list(moments[0].keys())
+                        adv = amputed_data[numerical_keys].values
+                        missing_mask = np.isnan(adv)
+                        idv = list(df[numerical_keys].values[missing_mask]
+                                   for df in imputed_data)
+                        mean_idv = np.mean(idv, axis=0)
+                        adv = adv[missing_mask]
+                        assert adv.shape == mean_idv.shape
+                        nrmse = (np.mean((adv-mean_idv)**2) / np.var(adv))**.5
+                        assert len(nrmse.shape) == 0
 
-                    if len(imputed_data) > 1:
-                        table[algo_name]['RMSE'][data_name][ampute_fun_name]\
-                            [str(proportion)] = utils.rmse(
-                                np.isnan(amputed_data.values),
-                                full_data.values,
-                                list(d.values for d in imputed_data))
+                        table[algo_name]['NRMSE'][norm_type][data_name]\
+                            [ampute_fun_name][str(proportion)] = nrmse
 
-    cols_1 = []
-    cols_2 = []
-    rows_1 = None
-    rows_2 = None
-    rows_3 = None
-    for algo in sorted(table.keys()):
-        cols_l = []
-        for measurement in sorted(table[algo].keys()):
-            cols_l.append(measurement)
-            _rows_1 = []
-            _rows_2 = []
-            _rows_3 = []
-            for data in sorted(table[algo][measurement]):
-                ampute_l = []
-                for ampute in sorted(table[algo][measurement][data]):
-                    proportion_l = list(sorted(table[algo][measurement][data][ampute].keys()))
-                    _rows_3 += proportion_l
-                    ampute_l += [ampute]*len(proportion_l)
-                _rows_2 += ampute_l
-                _rows_1 += [data]*len(ampute_l)
-            if rows_1 is None:
-                rows_1 = _rows_1
-                rows_2 = _rows_2
-                rows_3 = _rows_3
-            else:
-                assert rows_1 == _rows_1
-                assert rows_2 == _rows_2
-                assert rows_3 == _rows_3
+                        # RMSE of 0-1 normalised data
+                        rmse_fd, rmse_ad, *rmse_id = utils.normalise_dataframes(
+                            full_data[numerical_keys],
+                            amputed_data[numerical_keys],
+                            *(i[numerical_keys] for i in imputed_data),
+                            method='min_max')[0]
 
+                        table[algo_name]['RMSE'][norm_type][data_name]\
+                            [ampute_fun_name][str(proportion)] = utils.mean_rmse(
+                                np.isnan(rmse_ad.values), rmse_fd.values,
+                                list(d.values for d in rmse_id))
 
-        cols_1 += [algo]*len(cols_l)
-        cols_2 += cols_l
+                        d = percentage_falsely_classified(
+                            amputed_data, full_data, imputed_data)
+                        for k, v in d.items():
+                            table[algo_name][k][norm_type][data_name]\
+                                [ampute_fun_name][str(proportion)] = v
 
-    data = -np.ones(shape=[len(rows_1), len(cols_1)], dtype=np.float)
-    for i, (r1, r2, r3) in enumerate(zip(rows_1, rows_2, rows_3)):
-        for j, (c1, c2) in enumerate(zip(cols_1, cols_2)):
-            data[i,j] = table[c1][c2][r1][r2][r3]
+    def get_multiindex(d, levels=3):
+        if levels == 1:
+            return [sorted(list(d.keys()))]
+        index = list([] for _ in range(levels))
+        for key in sorted(d.keys()):
+            item = d[key]
+            lower_levels = get_multiindex(item, levels=levels-1)
+            for i in range(levels-1):
+                index[i+1] += lower_levels[i]
+            index[0] += [key]*len(lower_levels[0])
+        return index
+
+    cols = get_multiindex(table, levels=2)
+    rows = None
+    for algo, measurement in zip(*cols):
+        _rows = get_multiindex(table[algo][measurement], levels=4)
+        if rows is None:
+            rows = _rows
+        assert rows == _rows, "All rows of the passed dict must be the same"
+
+    data = np.empty(shape=[len(rows[0]), len(cols[0])], dtype=np.float)
+    data[...] = np.inf  # Detect errors when looking at the table
+    for i, (r1, r2, r3, r4) in enumerate(zip(*rows)):
+        for j, (c1, c2) in enumerate(zip(*cols)):
+            data[i,j] = table[c1][c2][r1][r2][r3][r4]
 
     return pd.DataFrame(data,
-                        index=[np.array(a) for a in [rows_1, rows_2, rows_3]],
-                        columns=[np.array(a) for a in [cols_1, cols_2]])
+                        index=[np.array(a) for a in rows],
+                        columns=[np.array(a) for a in cols])
+
+
+def dataframe_like(dataframe, new_values):
+    return pd.DataFrame(new_values, index=dataframe.index,
+                        columns=dataframe.columns)
+
+
+class TestPFC(unittest.TestCase):
+    def setUp(self):
+        self.full_data = pd.DataFrame({
+            'i': [1,2,3],
+            'c': ['a', 'b', 'c']})
+        self.full_data.i = self.full_data.i.astype(np.int32)
+        self.full_data.c = self.full_data.c.astype('category')
+    def test_pfc_correct(self):
+        amputed_data = self.full_data.copy()
+        amputed_data.loc[0, 'c'] = np.nan
+        amputed_data.loc[0, 'i'] = utils.NA_INT32
+        imputed_data = [self.full_data.copy()]
+        d = percentage_falsely_classified(
+            amputed_data, self.full_data,
+            imputed_data)
+        self.assertEqual(d['PFC'], 0.0)
+        self.assertEqual(d['PFC_cat'], 0.0)
+        self.assertEqual(d['PFC_int'], 0.0)
+
+    def test_pfc_individual(self):
+        amputed_data = self.full_data.copy()
+        amputed_data.loc[0, 'c'] = np.nan
+        amputed_data.loc[0, 'i'] = utils.NA_INT32
+
+        imputed_data = [self.full_data.copy()]
+        imputed_data[0].loc[0, 'c'] = 'b'
+        imputed_data[0].loc[2, 'c'] = 'b'
+        imputed_data[0].loc[0, 'i'] = 1
+        imputed_data[0].loc[2, 'i'] = 1
+        print(amputed_data)
+        print(self.full_data)
+        print(imputed_data)
+        d = percentage_falsely_classified(
+            amputed_data, self.full_data,
+            imputed_data)
+        self.assertEqual(d['PFC'], 0.5)
+        self.assertEqual(d['PFC_cat'], 0.)
+        self.assertEqual(d['PFC_int'], 1.)
+
 
 if __name__ == '__main__':
-    print(benchmark({'MICE': utils.impute_mice, 'MissForest': utils.impute_missforest}, datasets()))
+    dsets = datasets()
+    baseline = benchmark({'MICE': memoize(utils.impute_mice),
+                          'MissForest': memoize(utils.impute_missforest)}, dsets)
--- timeless-imputation/utils.py
+++ timeless-imputation/utils.py
@@ -1,45 +1,80 @@
 import numpy as np
+import pandas as pd
 from rpy2.robjects.packages import importr
 import rpy2.robjects
 import rpy2.robjects.pandas2ri
 import rpy2.robjects.numpy2ri
+
 rpy2.robjects.pandas2ri.activate()
 rpy2.robjects.numpy2ri.activate()
-
 py2ri = rpy2.robjects.pandas2ri.py2ri
 ri2py = rpy2.robjects.pandas2ri.ri2py
+R = rpy2.robjects.r
 
+base = importr('base')
+base = importr('base')
 mice = importr('mice')
 missForest = importr('missForest')
 doParallel = importr('doParallel')
 parallel = importr('parallel')
+
 n_cores = parallel.detectCores()
 doParallel.registerDoParallel(parallel.makeCluster(n_cores))
 
 __all__ = ['rmse', 'mean_rmse', 'gondara_rmse_sum', 'mcar_missing_rows',
            'mcar_missing_total', 'normalise_dataframes', 'impute_mice',
-           'impute_missforest']
+           'impute_missforest', 'df_py2ri', 'NA_INT32']
+
+NA_INT32 = -(1 << 31)
+
+
+def df_py2ri(df):
+    ordered_keys = filter(lambda k: df[k].dtype == np.int32, df.keys())
+    return base.transform(df, **dict((k, base.as_ordered(df[k]))
+                                     for k in ordered_keys))
+
+
+def df_ri2py(rdf):
+    df = ri2py(rdf)
+    for k in df.keys():
+        assert df[k].dtype not in [np.float32, np.int64], \
+            "f32 and i64 not handled"
+        if df[k].dtype == np.object:
+            df[k] = df[k].astype('category')
+        if df[k].dtype == np.int32:
+            assert not np.any(df[k].values == 0), \
+                "Ordered variables should start at 1"
+            df[k].values[df[k].values < 0] = 0
+    return df
+
 
 def rmse(mask_missing, original_df, multiple_imputed_df):
     "Compute the RMSE between a dataset and its imputations"
-    assert original_df.shape == multiple_imputed_df[0].shape, "data set shape not matching"
+    assert original_df.shape == multiple_imputed_df[0].shape, \
+        "data set shape not matching"
     sq_diff = (original_df-multiple_imputed_df)**2
     mse = np.sum(sq_diff*mask_missing)/np.sum(mask_missing)
     return mse**.5
 
+
 def mean_rmse(mask_missing, original_df, multiple_imputed_df):
-    "Compute RMSE of the optimal point according to the multiple-imputed distribution"
+    """Compute RMSE of the optimal point according to the multiple-imputed
+    distribution"""
     midf = np.mean(multiple_imputed_df, axis=0, keepdims=True)
     return rmse(mask_missing, original_df, midf)
 
+
 def gondara_rmse_sum(mask_missing, original_df, multiple_imputed_df):
-    "RMSE_sum as in Multiple Imputation Using Deep Denoising Autoencoders (Gondara & Wang 2017)"
-    assert original_df.shape == multiple_imputed_df[0].shape, "data set shape not matching"
+    """RMSE_sum as in Multiple Imputation Using Deep Denoising Autoencoders
+    (Gondara & Wang 2017)"""
+    assert original_df.shape == multiple_imputed_df[0].shape, \
+        "data set shape not matching"
     sq_diff = (original_df-multiple_imputed_df)**2
-    sq_diff[:,~mask_missing] = 0
+    sq_diff[:, ~mask_missing] = 0
     per_attribute_rmse = np.mean(np.sum(sq_diff, axis=1), axis=0)**.5
     assert per_attribute_rmse.shape == (original_df.shape[1],)
     return np.sum(per_attribute_rmse)
+
 
 def mcar_rows_gondara(dataset_, rows_missing=0.2, missing_row_loss=0.5):
     dataset = dataset_.copy()
@@ -52,49 +87,99 @@ def mcar_rows_gondara(dataset_, rows_missing=0.2, missing_row_loss=0.5):
             d = dataset.values
         else:
             d = dataset
-        d[i,r[:int(r.shape[0]*missing_row_loss)]] = np.nan
+        d[i, r[:int(r.shape[0]*missing_row_loss)]] = np.nan
     return dataset
 
-def mcar_rows(dataset_, rows_missing=0.2, missing_row_loss=0.5):
-    dataset = dataset_.copy()
-    missing = np.random.rand(len(dataset)) < rows_missing
-    for i in np.nonzero(missing)[0]:
-        row_missing = np.random.rand(dataset.shape[1]) < missing_row_loss
-        if hasattr(dataset, 'values'):
-            d = dataset.values
-        else:
-            d = dataset
-        d[i,row_missing] = np.nan
-    return dataset
 
-def mcar_total(dataset_, missing_proportion=0.5):
+def _type_aware_drop(dataset, missing_mask):
+    assert isinstance(dataset, pd.DataFrame)
+    int_keys = list(filter(lambda k: dataset[k].dtype == np.int32,
+                           dataset.keys()))
+    float_keys = list(filter(lambda k: dataset[k].dtype == np.float64,
+                             dataset.keys()))
+    cat_keys = list(filter(lambda k: dataset[k].dtype.name == 'category',
+                           dataset.keys()))
+    m_df = pd.DataFrame(missing_mask, columns=list(dataset.keys()))
+    df_cats = dataset[cat_keys].copy()
+    for idx in zip(*np.nonzero(m_df[cat_keys].values)):
+        df_cats.iloc[idx] = None
+    df = pd.concat([
+        dataset[int_keys].where(m_df[int_keys].values, other=NA_INT32),
+        dataset[float_keys].where(m_df[float_keys].values),
+        df_cats,
+        ], axis=1)
+    return df[dataset.keys()]
+
+
+def mcar_rows(dataset, rows_missing=0.2, missing_row_loss=0.5):
+    """Make each row missing with probability `rows_missing`. Rows that are
+    missing have a `missing_row_loss` probability of having each of their
+    elements missing. Thus the proportion of missing values in total is
+    `rows_missing * missing_row_loss`"""
+    example_mask = np.random.rand(dataset.shape[0]) < rows_missing
+    row_mask = np.random.rand(*dataset.shape) < missing_row_loss
+    overall_mask = example_mask[:, np.newaxis]
+    return _type_aware_drop(dataset, row_mask*overall_mask)
+
+
+def mcar_total(dataset, missing_proportion=0.5):
     "Make each cell of the dataset missing uniformly at random"
-    dataset = dataset_.copy()
-    missing = np.random.rand(*dataset.shape) < missing_proportion
-    if hasattr(dataset, 'values'):
-        d = dataset.values
+    return _type_aware_drop(dataset,
+                            np.random.rand(*dataset.shape) < missing_proportion)
+
+
+def _rescale_dataframes(dataframes, mean, std, rescale_f):
+    keys = list(mean.keys())
+    rescaled_dfs = list(pd.concat([
+        rescale_f(df[keys], mean, std),
+        df.drop(keys, axis=1)], axis=1) for df in dataframes)
+    return rescaled_dfs
+
+
+def normalise_dataframes(*dataframes, method='mean_std'):
+    """Normalise all passed dataframes with the mean and std of the first, or
+    by the min and max of the first."""
+    df = dataframes[0]
+    assert all(sorted(df.keys()) == sorted(a.keys())
+               for a in dataframes), "All dataframes must have the same columns"
+    numerical_columns = list(filter(lambda k: df[k].dtype == np.float64,
+                                    df.keys()))
+    if method == 'mean_std':
+        mean = df[numerical_columns].mean(axis=0)
+        std = df[numerical_columns].std(axis=0)
+    elif method == 'min_max':
+        mean = df[numerical_columns].min(axis=0)
+        std = df[numerical_columns].max(axis=0) - mean
     else:
-        d = dataset
-    d[missing] = np.nan
-    return dataset
+        raise ValueError("`method` must be one of ('min_max', 'mean_std')")
+    return (_rescale_dataframes(dataframes, mean, std, lambda d, m, s: (d-m)/s),
+            (mean, std))
 
-def normalise_dataframes(*dataframes):
-    "Normalise all passed dataframes with the mean and std of the first"
-    mean = dataframes[0].mean()
-    std = dataframes[0].std()
-    return tuple(map(lambda d: (d-mean)/std, dataframes))
 
-def impute_mice(dataset, number_imputations=5):
+def unnormalise_dataframes(mean_std, dataframes):
+    """Return the dataframes to their original size using the information in
+    `mean_std`"""
+    mean, std = mean_std
+    assert all(isinstance(df, pd.DataFrame) for df in dataframes)
+    return _rescale_dataframes(dataframes, mean, std, lambda d, m, s: d*s+m)
+
+
+def impute_mice(dataset, number_imputations=5, full_data=None):
     "Imputed dataset using MICE"
-    df = py2ri(dataset)
+    del full_data
+    df = df_py2ri(dataset)
     obj = mice.mice(df, m=number_imputations, maxit=100, method='pmm', seed=500)
-    return list(ri2py(mice.complete(obj, v)) for v in range(1, number_imputations+1))
+    return list(ri2py(mice.complete(obj, v))
+                for v in range(1, number_imputations + 1))
 
-def impute_missforest(dataset, number_imputations=1):
-    assert number_imputations==1
+
+def impute_missforest(dataset, number_imputations=1, full_data=None):
+    del full_data
+    assert number_imputations == 1
     if dataset.shape[1] < n_cores[0]:
         par = 'no'
     else:
         par = 'variables'
-    mf_imp = missForest.missForest(dataset, parallelize=par)
+    df = df_py2ri(dataset)
+    mf_imp = missForest.missForest(df, parallelize=par)
     return [ri2py(mf_imp[0])]
